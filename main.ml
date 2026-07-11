fdd3059964984049f5672df13ac67a9321f2ef0c
Nikita Egorov <avelparmen@gmail.com>
2026-07-11 15:33:58 +0300

    refactor: Add context saving for "parse_program", fix warnings, .. .. simplify program

--- main.ml
+++ main.ml
@@ -1,4 +1,4 @@
-type oper = Add | Multiply | Divide
+type oper = Add | Subtract | Multiply | Divide
 type atomic = 
 | Const of int
 | Register of string
@@ -8,7 +8,7 @@ type cmp_sign =
 | LESS_EQUAL
 
 type action =
-| Binop of oper * atomic * atomic
+(* | Binop of oper * atomic * atomic *)
 | LetR of string * string * oper * string
 | LetI of string * string * oper * int
 
@@ -24,19 +24,10 @@ type nested =
 | Function of string * string * nested (* function name, argument, function body *)
 | SingleNested of action
 
-
-(* let example = Binop(Add, Const 5, Binop(Multiply, Const 5, Const 6))  *)
-
-(* let var = Let("my_var", 15);; *)
-
-(* let print_var (v : expr) =
-  match v with
-  | Let(str, num) -> 
-      print_endline (str ^ ": " ^ string_of_int num)
-  | _ -> failwith "Error";; *)
-
-
-(*print_var var;;*)
+type compile_context = {
+  function_name : string;
+}
+let start_context = {function_name = "_start"}
 
 type instr = 
 | LABEL of string
@@ -46,6 +37,7 @@ type instr =
 | ADD of string * string * string
 | SUB of string * string * string
 | MUL of string * string * string
+| DIV of string * string * string
 | ADDI of string * string * int
 | SD of string * int * string
 | LD of string * int * string
@@ -61,6 +53,7 @@ let str_of_instr = function
 | ADD(rd, rs1, rs2) -> Printf.sprintf "add %s, %s, %s" rd rs1 rs2
 | SUB(rd, rs1, rs2) -> Printf.sprintf "sub %s, %s, %s" rd rs1 rs2
 | MUL(rd, rs1, rs2) -> Printf.sprintf "mul %s, %s, %s" rd rs1 rs2
+| DIV(rd, rs1, rs2) -> Printf.sprintf "div %s, %s, %s" rd rs1 rs2
 | ADDI(rd, rs, imm) -> Printf.sprintf "addi %s, %s, %d" rd rs imm
 | SD(rs, shift, addr) -> Printf.sprintf "sd %s, %d(%s)" rs shift addr
 | LD(rd, shift, addr) -> Printf.sprintf "ld %s, %d(%s)" rd shift addr
@@ -73,26 +66,37 @@ let str_of_instr_w v =
   | MV(rd, rs) -> if rd <> rs then "\t" ^ str_of_instr v ^ "\n" else ""
   | _ -> "\t" ^ str_of_instr v ^ "\n";;
 
-let rec str_of_action = function
+let rec str_of_action (context : compile_context) = function
 | LetI(rd, rs1, op, imm) ->
     begin 
       match op with
-      | Add -> str_of_instr_w(ADDI(rd, rs1, imm))
+      | Add ->      str_of_instr_w (ADDI(rd, rs1, imm))
+      | Subtract -> str_of_instr_w (LI("t0", imm)) ^
+                    str_of_instr_w (SUB(rd, rs1, "t0"))
+      | Multiply -> str_of_instr_w (LI("t0", imm)) ^
+                    str_of_instr_w (MUL(rd, rs1, "t0"))
+      | Divide ->   str_of_instr_w (LI("t0", imm)) ^
+                    str_of_instr_w (DIV(rd, rs1, "t0"))
     end
 | LetR(rd, rs1, op, rs2) ->
     begin
       match op with
-      | Add -> str_of_instr_w (ADD(rd, rs1, rs2))
+      | Add ->      str_of_instr_w (ADD(rd, rs1, rs2))
+      | Subtract -> str_of_instr_w (SUB(rd, rs1, rs2))
+                    
       | Multiply -> str_of_instr_w (MUL(rd, rs1, rs2))
+      | Divide ->   str_of_instr_w (DIV(rd, rs1, rs2))
     end
 | Putarg(rs) -> str_of_instr_w (MV("a0", rs))
-| Return(rs) -> str_of_action (Putarg rs) ^ str_of_instr_w (J "fin1")
+| Return(rs) -> str_of_action context (Putarg rs) ^ 
+                str_of_instr_w (J (context.function_name ^ "_fin"))
 | Call(name) -> 
     let save_callee = str_of_instr_w (SD ("ra", 0, "sp")) in
     let call_str = str_of_instr_w (CALL(name)) in
     let restore_callee = str_of_instr_w (LD ("ra", 0, "sp")) in
     save_callee ^ call_str ^ restore_callee
-| Ecall -> "\tecall\n";;
+| Ecall -> "\tecall\n"
+| _ -> failwith "Error: Undefined action!";;
 
 let str_of_condition condition label =
   match condition with
@@ -104,36 +108,35 @@ let str_of_condition condition label =
       | LESS_EQUAL -> BGE("t0", rs1, label) in
       str_of_instr_w temp_inst ^ str_of_instr_w next_inst;;
     
-let rec parse_program = function
+let rec parse_program (context : compile_context) = function
 | Function(func_name, arg, seq) -> 
+    let new_context = {context with function_name=func_name} in
     let label = str_of_instr_w (LABEL func_name) in
-    let st =  str_of_instr_w (ADDI ("sp", "sp", -16)) (* ^ 
-              str_of_instr_w (SD (arg, 8, "sp")) ^
-              str_of_instr_w (SD ("ra", 0, "sp")) *) in 
-    let ed =  str_of_instr_w (ADDI ("sp", "sp", 16)) (* ^ 
-              str_of_instr_w (LD (arg, 8, "sp")) ^
-              str_of_instr_w (LD ("ra", 0, "sp")) *) in 
+    let st = str_of_instr_w (ADDI ("sp", "sp", -16)) in 
+    let ed = str_of_instr_w (ADDI ("sp", "sp", 16)) in 
     let return = str_of_instr_w RET in
-    let imm = parse_program seq in
+    let imm = parse_program new_context seq in
     label ^ st ^ imm ^ ed ^ return
 | Sequence(curr, next_seq) ->
-  let next_seq = parse_program next_seq in
-    str_of_action curr ^ next_seq
+  let next_seq = parse_program context next_seq in
+    str_of_action context curr ^ next_seq
 | If (condition, thn, els) ->
     begin  
     match condition with
     Condition (c_rs1, c_sign, c_imm) ->
-      let then_label = str_of_instr_w (LABEL("then1")) in
-      let fin_label = str_of_instr_w (LABEL("fin1")) in
+      let then_name = context.function_name ^ "_then" in
+      let fin_name = context.function_name ^ "_fin" in
+      let then_label = str_of_instr_w (LABEL then_name) in
+      let fin_label = str_of_instr_w (LABEL fin_name) in
     (*let jump_fin = str_of_instr_w (J("then1")) in *)
       let cond = (c_rs1, c_sign, c_imm) in
-      let cond_str = (str_of_condition cond "then1") in
-      let thn_str = parse_program thn in
-      let els_str = parse_program els in
+      let cond_str = (str_of_condition cond then_name) in
+      let thn_str = parse_program context thn in
+      let els_str = parse_program context els in
       cond_str ^ els_str (*^ jump_fin *) ^ then_label ^ thn_str ^ fin_label
     | _ -> failwith "ERROR: Expected condition in if-clause, but actual is not."
     end
-| SingleNested(action) -> str_of_action action;;
+| SingleNested(action) -> str_of_action context action;;
 
 
 let start = 
@@ -171,7 +174,7 @@ Function("factorial", "a0",
   )
 );;
 
-print_string (parse_program start ^ parse_program program);;
+print_string (parse_program start_context start ^ parse_program start_context program);;
 
 let a () =
   print_string (str_of_instr_w (LABEL("fact")));
